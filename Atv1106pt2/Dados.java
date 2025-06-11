a4f8dd7561f238df7afbf4400741f8d60f9ca36e
Caioba123 <caiomuller7192@gmail.com>
2025-06-11 08:55:43 -0300

    Update Dados.java

--- Atv1106pt2/Dados.java
+++ Atv1106pt2/Dados.java
@@ -13,39 +13,34 @@ public class Dados {
         int escolha = teclado.nextInt();
       
 
-        if (escolha == 1) {
-            System.out.print("\n  Nome do dono do animal é Jõao ");
-            System.out.print("\n Numero de telefone do dono é 9 8216 8273 ");
-            System.out.print("\n Nome do animal é Rex ");
-            System.out.print("\n Especie do animal é Cachorro ");
-            System.out.print("\n Idade do animal é 2 anos ");
-        } else if (escolha == 2) {
-        	System.out.print("\n  Nome do dono do animal é Maria ");
-            System.out.print("\n Numero de telefone do dono é 9 9876 5432 ");
-            System.out.print("\n Nome do animal é Luna ");
-            System.out.print("\n Especie do animal é Gato ");
-            System.out.print("\n Idade do animal é 4 anos ");
-        } else if (escolha == 3) {
-        	System.out.print("\n  Nome do dono do animal é Carlos ");
-            System.out.print("\n Numero de telefone do dono é 9 9123 4567 ");
-            System.out.print("\n Nome do animal é Thor ");
-            System.out.print("\n Especie do animal é Cachorro ");
-            System.out.print("\n Idade do animal é 3 anos ");
-        } else if (escolha == 4) {
-        	System.out.print("\n  Nome do dono do animal é Ana ");
-            System.out.print("\n Numero de telefone do dono é 9 9988 7766 ");
-            System.out.print("\n Nome do animal é Mel ");
-            System.out.print("\n Especie do animal é Coelho ");
-            System.out.print("\n Idade do animal é 1 ano ");
-        } else if (escolha == 5) {
-        	System.out.print("\n  Nome do dono do animal é Pedro ");
-            System.out.print("\n Numero de telefone do dono é 9 9345 6789 ");
-            System.out.print("\n Nome do animal é Spike ");
-            System.out.print("\n Especie do animal é Papagaio ");
-            System.out.print("\n Idade do animal é 5 anos ");
-        } else {
-            System.out.println("Seção inválida.");
-        }
+       } else if (escolha == 2) {
+    System.out.print("\n Nome do dono do animal é Juliana ");
+    System.out.print("\n Número de telefone do dono é 9 8765 4321 ");
+    System.out.print("\n Nome do animal é Bella ");
+    System.out.print("\n Espécie do animal é Gato ");
+    System.out.print("\n Idade do animal é 2 anos ");
+    } else if (escolha == 3) {
+    System.out.print("\n Nome do dono do animal é Rafael ");
+    System.out.print("\n Número de telefone do dono é 9 9123 9876 ");
+    System.out.print("\n Nome do animal é Max ");
+    System.out.print("\n Espécie do animal é Cachorro ");
+    System.out.print("\n Idade do animal é 6 anos ");
+    } else if (escolha == 4) {
+    System.out.print("\n Nome do dono do animal é Camila ");
+    System.out.print("\n Número de telefone do dono é 9 9456 7890 ");
+    System.out.print("\n Nome do animal é Thor ");
+    System.out.print("\n Espécie do animal é Coelho ");
+    System.out.print("\n Idade do animal é 3 anos ");
+    } else if (escolha == 5) {
+    System.out.print("\n Nome do dono do animal é Lucas ");
+    System.out.print("\n Número de telefone do dono é 9 9345 6788 ");
+    System.out.print("\n Nome do animal é Kiwi ");
+    System.out.print("\n Espécie do animal é Papagaio ");
+    System.out.print("\n Idade do animal é 4 anos ");
+   } else {
+    System.out.println("Seção inválida.");
+  }
+
         
         System.out.println("\n\nDigite 1 para voltar ao menu:");
         int voltar = teclado.nextInt();
